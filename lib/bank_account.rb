3363bb02399fca06bf22c28d099026ab802d185d
mmcneeley <mmcneeley@gmail.com>
2018-03-15 15:48:31 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class BankAccount
 
+  #do these need to be writers as well?
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    #binding.pry
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
 end
+
+
+#Avi.display_balance
